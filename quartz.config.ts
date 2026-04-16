888b54612c8fac365b27b7240cbaa32b159c2d40
surajgoraicse <surajgoraicse@gmail.com>
2026-04-16 23:10:23 +0530

    Quartz sync: Apr 16, 2026, 11:10 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "surajgoraicse.github.io/dev-notes-remote",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
